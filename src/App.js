d8c7d9f09ec890883c94b7015934ff11cc13eb6d
changwoo <11changwoo>
2022-06-02 15:41:38 +0900

    props,state,bind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-// import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 
 class App extends Component {
+
+  //생성자 호출시 제일 먼저 호출됨
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode:'read',
+      subject: {title:'WEB', sub:'World Wide Web!'},
+      welcome: {title:'Welcome', desc:'Hello, React!!'},
+      contents: [
+        {id:1, title:'HTML', desc:"HTML is for information"},
+        {id:2, title:'CSS', desc:"CSS is for design"},
+        {id:3, title:'JavaScript', desc:"JavaScript is for interactive"}
+      ]
+    };
+  }
+
   render() {
+    var _title, _desc = null;
+
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        Hello, Fucking word ~
+        {/* <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject> */}
+        <header>
+            <h1><a href="/" onClick = {function(e) {
+              e.preventDefault();
+              //컴포넌트 생성 후 동적으로 값을 변경시에는 setState() 사용
+              this.setState({
+                mode:'welcome'
+              });
+            }.bind(this) }>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
